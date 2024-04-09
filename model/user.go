93ecfc01c70ee21e24a560ce5fdef0e5d09b2417
yoyo <hsjejxys@gmail.com>
2024-04-09 20:02:04 +0900

    Userテーブルを定義

---
+++ model/user.go
@@ -0,0 +1,11 @@
+package model
+
+import "time"
+
+type User struct {
+	ID uint `json:"id" gorm:"primaryKey"`
+	Email string `json:"email" gorm:"unique"`
+	Password string `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
