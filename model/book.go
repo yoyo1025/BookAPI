45d2a2a23c521ae33528f7bc109e949ca281cf7f
yoyo <hsjejxys@gmail.com>
2024-04-09 20:24:57 +0900

    BookResponseテーブルを定義

--- model/book.go
+++ model/book.go
@@ -12,3 +12,10 @@ type Book struct {
 	User User `json:"user" gorm:"foreignKey:UserId; constraint:OnDelete:CASCADE"` 
 	UserId uint `json:"user_id" gorm:"not null"`
 }
+
+type BookResponse struct {
+	ID uint `json:"id" gorm:"primaryKey"`
+	Title string `json:"title" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
