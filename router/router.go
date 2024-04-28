1f6523da337a676911b2c098d1618bdef6ea8fc8
yoyo <hsjejxys@gmail.com>
2024-04-29 01:17:59 +0900

    グループにしてミドルウェアを追加

--- router/router.go
+++ router/router.go
@@ -2,7 +2,9 @@ package router
 
 import (
 	"BookAPI/controller"
+	"os"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
@@ -11,5 +13,10 @@ func NewRouter(uc controller.IUserController) *echo.Echo {
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
+	t := e.Group("/books")
+	t.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey: []byte(os.Getenv("SELECT")),
+		TokenLookup: "cookie:token",
+	}))
 	return e
 }
