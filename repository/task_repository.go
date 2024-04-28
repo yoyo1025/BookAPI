1d1125c83e4e5b47b8580a10671888b2cec701e8
yoyo <hsjejxys@gmail.com>
2024-04-28 18:41:28 +0900

    bookRepositoryモデルを定義

--- repository/task_repository.go
+++ repository/task_repository.go
@@ -1,6 +1,10 @@
 package repository
 
-import "BookAPI/model"
+import (
+	"BookAPI/model"
+
+	"gorm.io/gorm"
+)
 
 type IBookRepository interface {
 	GetAllBooks(books *[]model.Book, userID uint) error
@@ -8,4 +12,8 @@ type IBookRepository interface {
 	CreateBook(book *model.Book) error
 	UpdateBook(book *model.Book, userId uint, bookId uint) error
 	DeleteBook(userId uint, bookId uint) error
+}
+
+type bookRepository struct {
+	db *gorm.DB
 }
