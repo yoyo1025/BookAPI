7939ad7ba63074af5ace521ccc00a01649786bd2
yoyo <hsjejxys@gmail.com>
2024-04-29 11:00:38 +0900

    BookValidateメソッドを定義

--- validator/task_validator.go
+++ validator/task_validator.go
@@ -1,13 +1,26 @@
 package validator
 
-import "BookAPI/model"
+import (
+	"BookAPI/model"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
 
 type IBookValidator interface {
-	BookValidate(book model.Book)
+	BookValidate(book model.Book) error
 }
 
 type bookValidator struct{}
 
 func NewBookValidator() IBookValidator {
 	return &bookValidator{}
+}
+
+func (bv *bookValidator) BookValidate(book model.Book) error {
+	return validation.ValidateStruct(&book,
+		validation.Field(
+			&book.Title,
+			validation.Required.Error("title is required"),
+			validation.RuneLength(1, 10).Error("limited max 10 char"),
+		))
 }
