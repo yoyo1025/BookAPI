78b8f9e934f7f3c1994d5a08e0aed8bcad8c9720
yoyo <hsjejxys@gmail.com>
2024-04-29 16:55:26 +0900

    bookモデルを修正

--- model/book.go
+++ model/book.go
@@ -5,7 +5,6 @@ import "time"
 type Book struct {
 	ID        uint   `json:"id" gorm:"primaryKey"`
 	Title     string `json:"title" gorm:"not null"`
-	Image     []byte `json:"image"`
 	Comment   string `json:"comment"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
